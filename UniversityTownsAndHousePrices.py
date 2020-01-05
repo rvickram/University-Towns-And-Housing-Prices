80bd4521436d0c71d615d23a6f2e7be5023b383e
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-01-05 14:39:32 -0500

    Initial commit

---
+++ UniversityTownsAndHousePrices.py
@@ -0,0 +1,11 @@
+# By Ryan Vickramasinghe
+
+# The following code will prove the hypothesis: "University towns have their mean housing prices less effected by recessions."
+# Data is pulled from the following locations:
+# - From the Zillow research data site there is housing data for the United States. In particular the datafile for all homes 
+#   at a city level, City_Zhvi_AllHomes.csv, has median home sale prices at a fine grained level.
+# - From the Wikipedia page on college towns is a list of university towns in the United States which has been copy and pasted 
+#   into the file university_towns.txt.
+# - From Bureau of Economic Analysis, US Department of Commerce, the GDP over time of the United States in current dollars 
+#   (use the chained value in 2009 dollars), in quarterly intervals, in the file gdplev.xls. For this assignment, only look at
+#    GDP data from the first quarter of 2000 onward.
