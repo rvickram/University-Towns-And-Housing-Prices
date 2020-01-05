106cc26d133ea20b26fc17bf2c8c538d3cd93553
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-01-05 18:38:29 -0500

    Clean, format and prepare the university towns file.

--- UniversityTownsAndHousePrices.py
+++ UniversityTownsAndHousePrices.py
@@ -9,3 +9,50 @@
 # - From Bureau of Economic Analysis, US Department of Commerce, the GDP over time of the United States in current dollars 
 #   (use the chained value in 2009 dollars), in quarterly intervals, in the file gdplev.xls. For this assignment, only look at
 #    GDP data from the first quarter of 2000 onward.
+
+
+# The following function will return
+
+import pandas as pd
+import numpy as np
+
+states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', \
+    'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska',\
+    'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', \
+    'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', \
+    'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan',\
+    'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', \
+    'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', \
+    'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', \
+    'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', \
+    'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California',\
+    'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', \
+    'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', \
+    'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
+
+# Cleans, formats and returns a DataFrame of towns states they are in, from the university_towns.txt file
+def get_list_of_university_towns():
+    uniTownsDf = pd.read_table('university_towns.txt',header=None)
+    uniTownsDf.columns = ['Col1']
+
+    # remove strings in brackets or parentheses
+    uniTownsDf['Col1'] = uniTownsDf['Col1'].str.replace(r"\(.*\)","").str.strip()
+    uniTownsDf['Col1'] = uniTownsDf['Col1'].str.replace(r"\[.*\]","").str.strip()
+
+    # iterate through the file and format correctly by appending to the cleanUniTownsDf list
+    state = ''
+    cleanUniTownsDf = []
+    for line in uniTownsDf['Col1']:
+        # convert line to a string
+        lineStr = str(line)
+        # check that the value is a state:
+        isState = lineStr in states.values()
+        # if this is a state, store it, if not, add to the list
+        if(isState):
+            state = lineStr
+        else:
+            cleanUniTownsDf.append((state, lineStr))
+        
+    return pd.DataFrame(cleanUniTownsDf, columns=['State', 'RegionName']).head(20)
+
+print(get_list_of_university_towns())
